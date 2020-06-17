f6b6b6103db99a98908f054c5e34549fa0e85059
John So <johnso@Johns-MBP.hsd1.wa.comcast.net>
2020-06-17 11:22:58 -0700

    oauth

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+//Adding modules to code
 const express = require("express");
+require("./services/passport");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
-});
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-//localhost:5000
