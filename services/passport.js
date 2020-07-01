2e2bb881aebbb9df028d7c72184a617cdf1db170
John So <johnso@Johns-MBP.hsd1.wa.comcast.net>
2020-07-01 11:11:13 -0700

    more tweaks

--- services/passport.js
+++ services/passport.js
@@ -26,10 +26,10 @@ passport.use(
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((existingUser) => {
         if (existingUser) {
-          //we already have a record with the given profile ID
+          // we already have a record with the given profile ID
           done(null, existingUser);
         } else {
-          //we don't hae a user record with this ID, make a new record
+          // we don't have a user record with this ID, make a new record!
           new User({ googleId: profile.id })
             .save()
             .then((user) => done(null, user));
