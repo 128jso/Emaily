0614f2c9dc77552e0042320caf603733197c9d22
johnso <johnhyunso@gmail.com>
2022-01-03 15:41:32 -0800

    Client changes

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,33 +1,33 @@
 // SurveyNew shows SurveyForm and SurveyForm Review
-import React, { Component } from "react";
-import { reduxForm } from "redux-form";
+import React, {Component} from "react";
+import {reduxForm} from "redux-form";
 import SurveyForm from "./SurveyForm";
 import SurveyFormReview from "./SurveyFormReview";
 
 class SurveyNew extends Component {
-  state = { showFormReview: false };
+    state = {showFormReview: false};
 
-  renderContent() {
-    if (this.state.showFormReview) {
-      return (
-        <SurveyFormReview
-          onCancel={() => this.setState({ showFormReview: false })}
-        />
-      );
+    renderContent() {
+        if (this.state.showFormReview) {
+            return (
+                <SurveyFormReview
+                    onCancel={() => this.setState({showFormReview: false})}
+                />
+            );
+        }
+
+        return (
+            <SurveyForm
+                onSurveySubmit={() => this.setState({showFormReview: true})}
+            />
+        );
     }
 
-    return (
-      <SurveyForm
-        onSurveySubmit={() => this.setState({ showFormReview: true })}
-      />
-    );
-  }
-
-  render() {
-    return <div>{this.renderContent()}</div>;
-  }
+    render() {
+        return <div>{this.renderContent()}</div>;
+    }
 }
 
 export default reduxForm({
-  form: "surveyForm",
+    form: "surveyForm",
 })(SurveyNew);
